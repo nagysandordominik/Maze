2e8bd4b48e0c9d1faebd3bd3f77b7658cffc2eab
nagysandordominik <nagysandordominik@icloud.com>
2021-10-01 19:31:39 +0200

    testing first shape

--- index.js
+++ index.js
@@ -13,3 +13,8 @@ const render = Render.create({
 
 Render.run(render);
 Runner.run(Runner.create(), engine);
+
+const shape = Bodies.rectangle(200, 200, 50, 50, {
+    isStatic: true
+});
+World.add(world, shape)
