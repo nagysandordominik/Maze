f02efad8edf69f4ed93072b904bfc9e69ebe7645
nagysandordominik <nagysandordominik@icloud.com>
2021-10-19 21:49:22 +0200

    disabling gravity in y direction

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { Engine, Render, Runner, World, Bodies, Body, MouseConstraint, Mouse } = Matter;
+const { Engine, Render, Runner, World, Bodies, Body, MouseConstraint, Mouse, Events } = Matter;
 
 const cells = 10;
 const width = 600;
@@ -166,6 +166,7 @@ const goal = Bodies.rectangle(
     unitLength * .7,
     unitLength * .7,
     {
+        label: 'goal',
         isStatic: true
     }
 );
@@ -173,7 +174,9 @@ World.add(world, goal);
 
 // Ball
 
-const ball = Bodies.circle(unitLength / 2, unitLength / 2, unitLength / 3);
+const ball = Bodies.circle(unitLength / 2, unitLength / 2, unitLength / 3, {
+    label: 'ball'
+});
 World.add(world, ball);
 
 document.addEventListener('keydown', event => {
@@ -194,4 +197,17 @@ document.addEventListener('keydown', event => {
     if (event.keyCode === 65) {
         Body.setVelocity(ball, { x: x - 5, y });
     }
+});
+
+// Win condition 
+Events.on(engine, 'collisionStart', event => {
+    event.pairs.forEach(collision => {
+        const labels = ['ball', 'goal'];
+    if  (
+        labels.includes(collision.bodyA.label) &&
+        labels.includes(collision.bodyB.label)
+    ) {
+        console.log('Win')
+    }
+    });
 });
